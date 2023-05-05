1f4a309602189a838739e0e38a88958d7a15377c
Rodrigo <rodri15578@gmail.com>
2023-05-05 14:26:46 -0300

    integrador tp codo a codo

---
+++ script.js
@@ -0,0 +1,105 @@
+/*!
+ * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
+ * Copyright 2011-2023 The Bootstrap Authors
+ * Licensed under the Creative Commons Attribution 3.0 Unported License.
+ */
+
+(() => {
+    'use strict'
+  
+    const storedTheme = localStorage.getItem('theme')
+  
+    const getPreferredTheme = () => {
+      if (storedTheme) {
+        return storedTheme
+      }
+  
+      return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    }
+  
+    const setTheme = function (theme) {
+      if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.documentElement.setAttribute('data-bs-theme', 'dark')
+      } else {
+        document.documentElement.setAttribute('data-bs-theme', theme)
+      }
+    }
+  
+    setTheme(getPreferredTheme())
+  
+    const showActiveTheme = (theme, focus = false) => {
+      const themeSwitcher = document.querySelector('#bd-theme')
+  
+      if (!themeSwitcher) {
+        return
+      }
+  
+      const themeSwitcherText = document.querySelector('#bd-theme-text')
+      const activeThemeIcon = document.querySelector('.theme-icon-active use')
+      const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
+      const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')
+  
+      document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
+        element.classList.remove('active')
+        element.setAttribute('aria-pressed', 'false')
+      })
+  
+      btnToActive.classList.add('active')
+      btnToActive.setAttribute('aria-pressed', 'true')
+      activeThemeIcon.setAttribute('href', svgOfActiveBtn)
+      const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`
+      themeSwitcher.setAttribute('aria-label', themeSwitcherLabel)
+  
+      if (focus) {
+        themeSwitcher.focus()
+      }
+    }
+  
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+      if (storedTheme !== 'light' || storedTheme !== 'dark') {
+        setTheme(getPreferredTheme())
+      }
+    })
+  
+    window.addEventListener('DOMContentLoaded', () => {
+      showActiveTheme(getPreferredTheme())
+  
+      document.querySelectorAll('[data-bs-theme-value]')
+        .forEach(toggle => {
+          toggle.addEventListener('click', () => {
+            const theme = toggle.getAttribute('data-bs-theme-value')
+            localStorage.setItem('theme', theme)
+            setTheme(theme)
+            showActiveTheme(theme, true)
+          })
+        })
+    })
+  })()
+
+  /*!
+ funcion de calculo para el formulario de tickets de compra
+ */
+
+
+  function calcularPrecioFinal() {
+
+    console.log("ingreso a calcularPrecioFinal");
+    // en cantEnt almaceno la cantidad de entradas que ingreso el usuario
+    let cantEnt = document.getElementById("cantEntradas").value;
+    console.log("cantEnt:" + cantEnt);
+
+    // en desc almaceno el tipo de usuario y su correspondiente descuento
+    let desc = document.getElementById("porcentajeDescuento").value;
+    console.log(desc);
+
+    // obtengo el descuento 
+    let descTotal = (cantEnt*1000)*desc/100;
+    console.log("descTotal:" + descTotal);
+
+    // resto el descuento del precio total
+    let precioFinal  =  (cantEnt*1000) - descTotal;  
+    console.log("precioFinal:" + precioFinal);
+
+    // coloco en el html el valor de las entradas menos el descuento
+    document.getElementById("precioFinal").value=precioFinal;
+}
